fadfc1136e77970f25905dfcc8f7a6302850df44
PAI-Peganov <artyompeganov428@gmail.com>
2025-11-23 21:41:54 +0500

    Reinclude-React

--- FiitFlow/WebFlow/obj/Debug/net9.0/WebFlow.AssemblyInfo.cs
+++ FiitFlow/WebFlow/obj/Debug/net9.0/WebFlow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63614655c381ff0f51918758c5d4b55defebba9d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc0ca6650312560d462813e4ed5655b483020a30")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
