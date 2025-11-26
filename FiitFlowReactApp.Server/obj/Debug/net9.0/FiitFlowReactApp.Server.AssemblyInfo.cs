b43c6fb9c727db2dc7b32638c53c0ae23d197842
PAI-Peganov <artyompeganov428@gmail.com>
2025-11-26 23:52:07 +0500

    jump1-2

--- FiitFlowReactApp.Server/obj/Debug/net9.0/FiitFlowReactApp.Server.AssemblyInfo.cs
+++ FiitFlowReactApp.Server/obj/Debug/net9.0/FiitFlowReactApp.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FiitFlowReactApp.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4ae7fc8a9587f0991840e62105e1780bbbd248d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20e32bde5d524e06e5751077fb6fd1b1abc5be46")]
 [assembly: System.Reflection.AssemblyProductAttribute("FiitFlowReactApp.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FiitFlowReactApp.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
