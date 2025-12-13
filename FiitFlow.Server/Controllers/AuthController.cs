16c8a3dd04496118e3b40514c930dbd4c0dea4c9
PAI-Peganov <artyompeganov428@gmail.com>
2025-12-13 22:44:19 +0500

    controllers try

--- FiitFlow.Server/Controllers/AuthController.cs
+++ FiitFlow.Server/Controllers/AuthController.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.AspNetCore.Cors;
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using FiitFlow.Repository;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace FiitFlowReactApp.Server.Controllers
 {
@@ -12,22 +16,46 @@ namespace FiitFlowReactApp.Server.Controllers
     {
         private readonly ILogger<AuthController> _logger;
         private readonly IConfiguration _configuration;
+        private readonly IStudentRepository _studentRepository;
+        private readonly IGroupRepository _groupRepository;
 
-        public AuthController(ILogger<AuthController> logger, IConfiguration configuration)
+        public AuthController(
+            ILogger<AuthController> logger,
+            IConfiguration configuration,
+            IStudentRepository studentRepository,
+            IGroupRepository groupRepository)
         {
             _logger = logger;
             _configuration = configuration;
+            _studentRepository = studentRepository;
+            _groupRepository = groupRepository;
         }
 
         [HttpPost("login")]
-        public IActionResult Post([FromBody] StudentLoginRequest request)
+        public async Task<IActionResult> Post([FromBody] StudentLoginRequest request)
         {
-            return Ok(string.Join("3453", new[] {
-                request.LastName,
-                request.FirstName,
-                request.DateTime.Date.ToString(),
-                request.Group
-            }).GetHashCode());
+            if (!CheckStudentData(request.FirstName, request.LastName, request.Group))
+                return BadRequest("Wrong format");
+            var hashId = await TryGetStudentGuid(request.FirstName, request.LastName, request.Group);
+            if (hashId == null)
+                return BadRequest("Undefind student");
+            return Ok(hashId);
+        }
+
+        private bool CheckStudentData(string firstName, string lastName, string group) =>
+            Regex.IsMatch(firstName, "^[А-ЯЁ][а-яё]*$") &&
+            Regex.IsMatch(lastName, "^[А-ЯЁ][а-яё]*$") &&
+            Regex.IsMatch(group, "^ФТ-[0-9]{3}-[0-9]$");
+
+        private async Task<int?> TryGetStudentGuid(string firstName, string lastName, string group)
+        {
+            var groupEntity = await _groupRepository.GetByTitleAsync(group.Substring(0, 6), int.Parse(group.Substring(7, 1)));
+            if (groupEntity == null)
+                return null;
+            var student = await _studentRepository.GetByNameAsync(lastName + " " + firstName, groupEntity.Id);
+            if (student == null)
+                return null;
+            return (student.Id.ToString() + groupEntity.Id.ToString()).GetHashCode();
         }
     }
 
